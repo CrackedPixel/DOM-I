4ffa0d5c0f02c1c17ca469c8612ead8d9d46d42c
crackedpixel <acarbajal721@live.com>
2019-06-17 14:53:58 -0500

    Copied original text

--- js/index.js
+++ js/index.js
@@ -40,3 +40,46 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Nav
+for (let i = 1; i < 7; ++i){
+  document.querySelector(`header nav a:nth-child(${i})`).textContent = siteContent.nav["nav-item-" + i];
+}
+// CTA
+//document.getElementById('logo-img').src = siteContent.nav["img-src"]; // already done by the demo
+document.querySelector('.cta-text h1').textContent = siteContent.cta.h1;
+document.querySelector('.cta-text button').textContent = siteContent.cta.button;
+document.getElementById('cta-img').src = siteContent.cta["img-src"];
+
+// Main
+document.querySelector('.top-content .text-content:nth-child(1) h4').textContent = siteContent["main-content"]["features-h4"];
+document.querySelector('.top-content .text-content:nth-child(1) p').textContent = siteContent["main-content"]["features-content"];
+document.querySelector('.top-content .text-content:nth-child(2) h4').textContent = siteContent["main-content"]["about-h4"];
+document.querySelector('.top-content .text-content:nth-child(2) p').textContent = siteContent["main-content"]["about-content"];
+document.getElementById('middle-img').src = siteContent["main-content"]["middle-img-src"];
+document.querySelector('.bottom-content .text-content:nth-child(1) h4').textContent = siteContent["main-content"]["services-h4"];
+document.querySelector('.bottom-content .text-content:nth-child(1) p').textContent = siteContent["main-content"]["services-content"];
+document.querySelector('.bottom-content .text-content:nth-child(2) h4').textContent = siteContent["main-content"]["product-h4"];
+document.querySelector('.bottom-content .text-content:nth-child(2) p').textContent = siteContent["main-content"]["product-content"];
+document.querySelector('.bottom-content .text-content:nth-child(3) h4').textContent = siteContent["main-content"]["vision-h4"];
+document.querySelector('.bottom-content .text-content:nth-child(3) p').textContent = siteContent["main-content"]["vision-content"];
+
+//Contact
+document.querySelector('.contact h4').textContent = siteContent["contact"]["contact-h4"];
+document.querySelector('.contact p:nth-child(2)').textContent = siteContent.contact.address;
+document.querySelector('.contact p:nth-child(3)').textContent = siteContent["contact"]["phone"];
+document.querySelector('.contact p:nth-child(4)').textContent = siteContent["contact"]["email"];
+
+//Footer
+document.querySelector('footer p').textContent = siteContent.footer.copyright;
+
+
+
+
+
+
+
+
+
+
+
